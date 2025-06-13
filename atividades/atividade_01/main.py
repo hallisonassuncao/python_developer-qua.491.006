adac845ba3cfc454c9fdb1361a932f5f7cf455d8
hallisonassuncao <hallison_assuncao@outlook.com>
2025-06-13 19:28:31 -0300

    Atividade 01

---
+++ atividades/atividade_01/main.py
@@ -0,0 +1,17 @@
+# tratamento de exceção
+while True:
+    try:
+        etanol = float(input("Digite o valor do etanol (R$): "))
+        gasolina = float(input("Digite o valor da gasolina (R$): "))
+        
+        combustivel = melhor_combustivel(etanol, gasolina)
+        print(f"O melhor combustível para abastecer é: {combustivel}")
+        
+        continuar = input("Deseja continuar? (s/n): ").strip().lower()
+        if continuar != 's':
+            break
+    except ValueError:
+        print("Por favor, digite valores válidos.")
+
+
+atividades/atividade_01
