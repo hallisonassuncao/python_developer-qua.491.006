0b845cc26b0950d6a65d7040bc6e875f58434596
hallisonassuncao <hallison_assuncao@outlook.com>
2025-06-13 19:51:32 -0300

    atividade 01 dia 12/06/2025

--- atividades/atividade_01/main.py
+++ atividades/atividade_01/main.py
@@ -1,11 +1,17 @@
 # tratamento de exceção
+def melhor_combustivel(etanol, gasolina):
+    if etanol / gasolina <= 0.7:
+        return "Etanol"
+    else:
+        return "Gasolina"
+
 while True:
     try:
         etanol = float(input("Digite o valor do etanol (R$): "))
         gasolina = float(input("Digite o valor da gasolina (R$): "))
         
         combustivel = melhor_combustivel(etanol, gasolina)
-        print(f"O melhor combustível para abastecer é: {combustivel}")
+        print(f"O melhor para abastecer é: {combustivel}")
         
         continuar = input("Deseja continuar? (s/n): ").strip().lower()
         if continuar != 's':
@@ -14,4 +20,4 @@ while True:
         print("Por favor, digite valores válidos.")
 
 
-atividades/atividade_01
+
