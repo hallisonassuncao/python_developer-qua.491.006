86cc3a588bef47a93727213860da158566fcf6e8
hallisonassuncao <hallison_assuncao@outlook.com>
2025-07-03 23:36:25 -0300

    Aula dia 03/07/2025

--- manipulacao_arquivos/50_json_parte_04/main.py
+++ manipulacao_arquivos/50_json_parte_04/main.py
@@ -1,4 +1,3 @@
-
 import json
 import os
 
@@ -7,43 +6,51 @@ novo_arquivo = ""
 
 while True:
     usuario = {}
-    print("1 - Cadastrar Novo Usuário. ")
-    print("2 - Salvar Arquivo Json. ")
-    print("3 - Fazer Leitura do Json. ")
-    print("4 - Sair do Programa. ")
+    print("1 - Cadastrar Novo Usuário.")
+    print("2 - Salvar Arquivo Json.")
+    print("3 - Fazer Leitura do Json.")
+    print("4 - Sair do Programa.")
     opcao = input("Informe a Opção Desejada: ")
     os.system("cls" if os.name == "nt" else "clear")
+
     match opcao:
         case "1":
             usuario['nome'] = input("Informe o Nome: ").strip().title()
             usuario['idade'] = int(input("Informe a Idade: "))
-            usuario['email'] = input("Informe o E-mail: ").strip().lower
+            usuario['email'] = input("Informe o E-mail: ").strip().lower()
 
             usuarios.append(usuario)
             os.system("cls" if os.name == "nt" else "clear")
-
-            print("Usuario Cadastrado com Sucesso.")
+            print("Usuário Cadastrado com Sucesso.")
             continue
+
         case "2":
-            novo_arquivo = input("Informe o Nome do Arquivo: ").strip().lower
-            with open(f"50_json_parte_04/{novo_arquivo}.json", "w", encoding="utf-8") as f:  # fazer uma correção desta linha 28
+            novo_arquivo = input("Informe o Nome do Arquivo: ").strip().lower()
+            with open(f"{novo_arquivo}.json", "w", encoding="utf-8") as f:
                 json.dump(usuarios, f, ensure_ascii=False, indent=4)
             os.system("cls" if os.name == "nt" else "clear")
             print("Arquivo Salvo com Sucesso.")
             continue
+
         case "3":
             if not novo_arquivo:
                 novo_arquivo = input("Informe o Nome do Arquivo: ").strip().lower()
-                with open(f"50_json_parte_04/{novo_arquivo}.json", "r", encoding="utf-8") as f:
+            try:
+                with open(f"{novo_arquivo}.json", "r", encoding="utf-8") as f:
                     dados = json.load(f)
                 print(f"{'-'*20} USUÁRIOS {'-'*20}\n")
                 for usuario in dados:
                     for chave in usuario:
                         print(f"{chave.capitalize()}: {usuario.get(chave)}")
                     print('-'*40)
-                    continue
+            except FileNotFoundError:
+                print("Arquivo não encontrado. Verifique o nome e tente novamente.")
+            continue
+
         case "4":
-            print("Programa Encerrado. ")
+            print("Programa Encerrado.")
+            break
+
         case _:
-            print("Opção Inválida. ")
+            print("Opção Inválida.")
             continue
