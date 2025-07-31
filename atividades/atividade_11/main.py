7d02a49575393dbbb1cf226c72e950b6d36f8b9c
hallisonassuncao <hallison_assuncao@outlook.com>
2025-07-30 21:10:40 -0300

    Aula dia 30/07/2025

--- atividades/atividade_11/main.py
+++ atividades/atividade_11/main.py
@@ -1,2 +1,9 @@
 # todo- atividade: crie um programa que receba do usuario um numero inteiro e o programa calcula o valor da sequencia
 # de fibonacci
+
+n = int(input("Digite um número: "))
+
+a, b = 0, 1
+for i in range(n):
+    print(a)
+    a, b = b, a + b
