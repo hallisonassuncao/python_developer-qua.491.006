c4dfdb1704405b1223fc0147b68849b4089ce07d
hallisonassuncao <hallison_assuncao@outlook.com>
2025-08-02 08:13:25 -0300

    Atividade dia 01/08/2025

--- atividades/atividade_13/main.py
+++ atividades/atividade_13/main.py
@@ -14,3 +14,41 @@
 # -Sacar valor
 # -Imprimir extrato (entendo-se: gerar arquivo com os dados da conta)
 # -Sair do programa.
+# NOTE - o nome da classe é conta
+
+# Primeiro, criamos a classe Conta
+class Conta:
+    def __init__(self, titular, cpf, agencia, numero):
+        self.titular = titular       # Nome da pessoa
+        self.cpf = cpf               # CPF da pessoa
+        self.agencia = agencia       # Número da agência
+        self.numero = numero         # Número da conta
+        self.saldo = 0.0             # Saldo começa zerado
+        self.movimentacoes = []      # Lista para guardar extrato
+
+    def consultar_dados(self):
+        print("---- Dados da Conta ----")
+        print("Titular:", self.titular)
+        print("CPF:", self.cpf)
+        print("Agência:", self.agencia)
+        print("Número da Conta:", self.numero)
+        print("Saldo: R$", self.saldo)
+
+    def depositar(self, valor):
+        self.saldo += valor
+        self.movimentacoes.append(f"Depósito de R${valor}")
+        print("Depósito feito com sucesso!")
+
+    def sacar(self, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor
+            self.movimentacoes.append(f"Saque de R${valor}")
+            print("Saque feito com sucesso!")
+        else:
+            print("Saldo insuficiente.")
+
+    def imprimir_extrato(self):
+        print("---- Extrato ----")
+        for item in self.movimentacoes:
+            print(item)
+        print("Saldo atual: R$", self.saldo)
