26e7506c5fe9d29e1c978101943464806c8e26b9
hallisonassuncao <hallison_assuncao@outlook.com>
2025-08-02 08:20:29 -0300

    Atividade dia 01/08/2025

--- atividades/atividade_13/main.py
+++ atividades/atividade_13/main.py
@@ -17,6 +17,8 @@
 # NOTE - o nome da classe é conta
 
 # Primeiro, criamos a classe Conta
+
+
 class Conta:
     def __init__(self, titular, cpf, agencia, numero):
         self.titular = titular       # Nome da pessoa
@@ -49,6 +51,43 @@ class Conta:
 
     def imprimir_extrato(self):
         print("---- Extrato ----")
+        for item in self.movimentacoes:
+            print(item)
+        print("Saldo atual: R$", self.saldo)
+
+        # Classe Conta 
+class Conta:
+    def __init__(self, titular, cpf, agencia, numero):
+        self.titular = titular
+        self.cpf = cpf
+        self.agencia = agencia
+        self.numero = numero
+        self.saldo = 0.0
+        self.movimentacoes = []
+
+    def consultar_dados(self):
+        print("\n---- Dados da Conta ----")
+        print("Titular:", self.titular)
+        print("CPF:", self.cpf)
+        print("Agência:", self.agencia)
+        print("Número da Conta:", self.numero)
+        print("Saldo: R$", self.saldo)
+
+    def depositar(self, valor):
+        self.saldo += valor
+        self.movimentacoes.append(f"Depósito: R${valor:.2f}")
+        print("Depósito realizado!")
+
+    def sacar(self, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor
+            self.movimentacoes.append(f"Saque: R${valor:.2f}")
+            print("Saque realizado!")
+        else:
+            print("Saldo insuficiente.")
+
+    def imprimir_extrato(self):
+        print("\n---- Extrato ----")
         for item in self.movimentacoes:
             print(item)
         print("Saldo atual: R$", self.saldo)
