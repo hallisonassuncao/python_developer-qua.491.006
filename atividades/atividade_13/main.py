b8f11501e390c48b26af015d7431ea4afe22a65d
hallisonassuncao <hallison_assuncao@outlook.com>
2025-08-01 21:14:54 -0300

    Aula dia 01/08/2025

---
+++ atividades/atividade_13/main.py
@@ -0,0 +1,16 @@
+# TODO - atividade: crie um programa de aplicativo de banco, onde :
+# - o Usuario cria uma conta no banco com os seguintes dados:
+
+# --Titular da conta
+# --Cpf do titular
+# --Agencia
+# --Numero da conta
+# --Saldo
+
+# O usuario pode fazer no programa:
+
+# -Consultar dados
+# -Depositar Valor
+# -Sacar valor
+# -Imprimir extrato (entendo-se: gerar arquivo com os dados da conta)
+# -Sair do programa.
