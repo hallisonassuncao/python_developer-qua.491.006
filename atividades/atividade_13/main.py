9511559eeedd13440b1ddfd7eccef1d5e88df3b9
hallisonassuncao <hallison_assuncao@outlook.com>
2025-08-02 08:22:46 -0300

    Atividade dia 01/08/2025

--- atividades/atividade_13/main.py
+++ atividades/atividade_13/main.py
@@ -55,39 +55,4 @@ class Conta:
             print(item)
         print("Saldo atual: R$", self.saldo)
 
-        # Classe Conta 
-class Conta:
-    def __init__(self, titular, cpf, agencia, numero):
-        self.titular = titular
-        self.cpf = cpf
-        self.agencia = agencia
-        self.numero = numero
-        self.saldo = 0.0
-        self.movimentacoes = []
-
-    def consultar_dados(self):
-        print("\n---- Dados da Conta ----")
-        print("Titular:", self.titular)
-        print("CPF:", self.cpf)
-        print("Agência:", self.agencia)
-        print("Número da Conta:", self.numero)
-        print("Saldo: R$", self.saldo)
-
-    def depositar(self, valor):
-        self.saldo += valor
-        self.movimentacoes.append(f"Depósito: R${valor:.2f}")
-        print("Depósito realizado!")
-
-    def sacar(self, valor):
-        if valor <= self.saldo:
-            self.saldo -= valor
-            self.movimentacoes.append(f"Saque: R${valor:.2f}")
-            print("Saque realizado!")
-        else:
-            print("Saldo insuficiente.")
-
-    def imprimir_extrato(self):
-        print("\n---- Extrato ----")
-        for item in self.movimentacoes:
-            print(item)
-        print("Saldo atual: R$", self.saldo)
+       
