3ca88d71fddb74a4d9c07d9d0e33ce1c9136d1bc
hallisonassuncao <hallison_assuncao@outlook>
2025-09-16 20:23:13 -0300

    Aula dia 15/09/2025

---
+++ 10_django_projeto_crud/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Pessoa(models.Model):
+    id_pessoa = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255, null=False, blank=False)
+    email = models.EmailField(unique=True, null=False, blank=False)
+    cpf = models.CharField(max_length=14, unique=True, null=False, blank=False)
+   
